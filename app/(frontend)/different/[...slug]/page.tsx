aa0cb6ec7d1f6daab8f68cd13879684137f992ec
Ugyen Tenzin <ugyen.tenzin@selise.ch>
2025-12-25 13:08:01 +0530

    feat(salesapp): package json

--- app/(frontend)/different/[...slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DifferentPage({ params }: { params: { slug: string[] } }) {
-    console.log(params);
-  return (
-    <div>
-      <h1>DifferentPage</h1>
-      <p>{params.slug}</p>
-    </div>
-  );
-}
